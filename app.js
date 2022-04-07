b12f1d62c30b49403207100cc6b5311c8f452024
joshdarling <joshua.a.darling@gmail.com>
2022-04-06 22:26:34 -0600

    App is displaying gifs based on users search results

---
+++ app.js
@@ -0,0 +1,41 @@
+console.log("Let's get this party started!");
+const form = document.querySelector('#search-form');
+const div = document.querySelector('#div-1');
+const input = document.querySelector('#search');
+const button = document.querySelector('#remove-button');
+
+
+async function getGiphy(q) {
+    let res = await axios.get(`https://api.giphy.com/v1/gifs/search?api_key=nbakhK1WSpBAs4yJTMyw0CkMzDGsDWQk&q=${q}&limit=25&offset=0&rating=g&lang=en`, {params:{q: q } } );
+    //console.log(res.data.data)
+    const img = document.createElement('img');
+    img.id = 'img';
+    for(let gif of res.data.data) {
+        //console.log(gif.images.downsized.url)
+        let nextImg = gif.images.downsized.url;
+        img.src = nextImg;
+
+    }
+   div.append(img)
+
+}
+
+
+
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+   getGiphy(input.value);
+   input.value = ''
+
+});
+
+button.addEventListener('click', function(){
+    div.remove();
+    
+    
+    //div.removeChild(img);
+})
+
+//getGiphy('dog');
